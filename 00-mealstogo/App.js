fc7dd1e0f689915d6fac32c7f4a44f86dacc03c6
uf-code <ugur.firat.developer@gmail.com>
2022-05-21 14:46:31 +0300

    adding search bar with react native paper

--- 00-mealstogo/App.js
+++ 00-mealstogo/App.js
@@ -1,16 +1,16 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native'
+import { Searchbar } from 'react-native-paper'
 
 // const isAndroid = Platform.OS === 'android'
-
-console.log(StatusBar.currentHeight)
+// console.log(StatusBar.currentHeight)
 
 export default function App() {
     return (
         <>
             <SafeAreaView style={styles.container}>
                 <View style={styles.search}>
-                    <Text>search</Text>
+                    <Searchbar placeholder='Search' />
                 </View>
                 <View style={styles.list}>
                     <Text>list</Text>
@@ -23,6 +23,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: { flex: 1, marginTop: StatusBar.currentHeight },
-    search: { padding: 16, backgroundColor: 'green' },
+    search: { padding: 16, backgroundColor: '#fff' },
     list: { flex: 1, padding: 16, backgroundColor: 'blue' },
 })
