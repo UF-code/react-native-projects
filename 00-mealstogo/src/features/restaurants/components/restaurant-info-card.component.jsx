3b2f43bd09cb1053497d66e26e44372e98c747ed
uf-code <ugur.firat.developer@gmail.com>
2022-05-22 23:36:14 +0300

    adding svg's to make nicer app views

--- 00-mealstogo/src/features/restaurants/components/restaurant-info-card.component.jsx
+++ 00-mealstogo/src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 import { Card } from 'react-native-paper'
+import { SvgXml } from 'react-native-svg'
+import star from '../../../../assets/star'
 
 const RestaurantCard = styled(Card)`
     background-color: ${(props) => props.theme.colors.bg.primary};
@@ -14,6 +16,19 @@ const Title = styled.Text`
     padding: ${(props) => props.theme.space[3]};
     color: ${(props) => props.theme.colors.ui.primary};
     font-family: ${(props) => props.theme.fonts.body};
+    font-size: ${(props) => props.theme.fontSizes.body};
+`
+const Info = styled(View)`
+    padding: ${(props) => props.theme.space[3]};
+`
+const Address = styled(Text)`
+    font-family: ${(props) => props.theme.fonts.heading};
+    font-size: ${(props) => props.theme.fontSizes.caption};
+`
+const Rating = styled(View)`
+    flex-direction: row;
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[2]};
 `
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -28,11 +43,23 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         rating = 4,
         isClosedTemporarily,
     } = restaurant
+
+    const ratingArray = Array.from(new Array(Math.floor(rating)))
+
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} style={styles.cover} />
 
-            <Title>{name}</Title>
+            <Info>
+                <Title>{name}</Title>
+                <Rating>
+                    {ratingArray.map(() => (
+                        <SvgXml xml={star} width={20} height={20} />
+                    ))}
+                </Rating>
+
+                <Address>{address}</Address>
+            </Info>
         </RestaurantCard>
     )
 }
