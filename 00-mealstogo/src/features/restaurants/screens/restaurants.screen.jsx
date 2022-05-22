787937464b13be49f0d9d95bd19eab08ce724aab
uf-code <ugur.firat.developer@gmail.com>
2022-05-22 13:12:41 +0300

    fixing ios bug

--- 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
+++ 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
@@ -8,7 +8,7 @@ import styled from 'styled-components/native'
 
 const SafeArea = styled(SafeAreaView)`
     flex: 1;
-    margin-top: ${StatusBar.currentHeight}px;
+    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
 `
 const SearchContainer = styled(View)`
     padding: 16px;
