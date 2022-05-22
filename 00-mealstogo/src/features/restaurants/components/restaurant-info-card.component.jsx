e52aed204f996966a37b6b193297c308bef7d6df
uf-code <ugur.firat.developer@gmail.com>
2022-05-22 12:13:44 +0300

    adding n testing styled-components

--- 00-mealstogo/src/features/restaurants/components/restaurant-info-card.component.jsx
+++ 00-mealstogo/src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import styled from 'styled-components/native'
 import { Text, StyleSheet } from 'react-native'
 import { Card } from 'react-native-paper'
+
+const Title = styled.Text`
+    padding: 16px;
+    color: red;
+`
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
     const {
@@ -19,6 +25,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
             <Card.Cover key={name} source={{ uri: photos[0] }} style={styles.cover} />
 
             <Text style={styles.title}>{name}</Text>
+            <Title>{name}</Title>
         </Card>
     )
 }
