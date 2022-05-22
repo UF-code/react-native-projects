d810a02848c913880ef017e0321af65435e7357f
uf-code <ugur.firat.developer@gmail.com>
2022-05-22 12:49:02 +0300

    implementing styled components

--- 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
+++ 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native'
 import { Searchbar } from 'react-native-paper'
-import { RestaurantInfoCard } from '../components/restaurant-info-card.component.jsx'
 // COMPONENTS
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component.jsx'
+//
+import styled from 'styled-components/native'
+
+const Search = styled(View)`
+    padding: 16px;
+    background-color: #fff;
+`
+const List = styled(View)`
+    flex: 1;
+    padding: 16px;
+    background-color: aqua;
+`
 
 // const isAndroid = Platform.OS === 'android'
 // console.log(StatusBar.currentHeight)
 
 export const RestaurantScreen = () => (
     <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
+        <Search>
             <Searchbar placeholder='Search' />
-        </View>
-        <View style={styles.list}>
+        </Search>
+        <List>
             <RestaurantInfoCard />
-        </View>
+        </List>
     </SafeAreaView>
 )
 
 const styles = StyleSheet.create({
     container: { flex: 1, marginTop: StatusBar.currentHeight },
-    search: { padding: 16, backgroundColor: '#fff' },
-    list: { flex: 1, padding: 16, backgroundColor: 'aqua' },
 })
