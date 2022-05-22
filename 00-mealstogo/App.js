544103bc6844276c947bbbd707dcd8462ea793a5
uf-code <ugur.firat.developer@gmail.com>
2022-05-22 15:35:41 +0300

    implementing theme

--- 00-mealstogo/App.js
+++ 00-mealstogo/App.js
@@ -1,12 +1,17 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
 // Screens
 import { RestaurantScreen } from './src/features/restaurants/screens/restaurants.screen.jsx'
+// Theme
+import { ThemeProvider } from 'styled-components/native'
+import { theme } from './src/infrastructure/theme/index.js'
 
 export default function App() {
     return (
         <>
-            <RestaurantScreen />
-            <ExpoStatusBar style='auto' />
+            <ThemeProvider theme={theme}>
+                <RestaurantScreen />
+                <ExpoStatusBar style='auto' />
+            </ThemeProvider>
         </>
     )
 }
