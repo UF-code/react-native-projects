d61bac21e6d6cd3aa554af8ae20486a8697a400c
uf-code <ugur.firat.developer@gmail.com>
2022-05-26 18:50:50 +0300

    adding loading widget

--- 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
+++ 00-mealstogo/src/features/restaurants/screens/restaurants.screen.jsx
@@ -8,6 +8,8 @@ import { SafeArea } from '../../../components/utility/safe-area.component.jsx'
 //
 import styled from 'styled-components/native'
 //
+import { ActivityIndicator, Colors } from 'react-native-paper'
+//
 import { RestaurantsContext } from '../../../services/restaurants/restaurants.context.jsx'
 
 const SearchContainer = styled.View`
@@ -24,14 +26,29 @@ const RestaurantList = styled(FlatList).attrs({
     },
 })``
 
+const Loading = styled(ActivityIndicator)`
+    margin-left: -25px;
+`
+const LoadingContainer = styled.View`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+`
+
 // const isAndroid = Platform.OS === 'android'
 // console.log(StatusBar.currentHeight)
 
 export const RestaurantScreen = () => {
     const { isLoading, error, restaurants } = useContext(RestaurantsContext)
-
+    if (isLoading) {
+    }
     return (
         <>
+            {isLoading && (
+                <LoadingContainer>
+                    <Loading size={50} animating={true} color={Colors.blue300} />
+                </LoadingContainer>
+            )}
             <SearchContainer>
                 <Searchbar placeholder='Search' />
             </SearchContainer>
