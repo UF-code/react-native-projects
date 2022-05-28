8cd07dc8969c136d3fdf586e3dc1ef96de21df9d
uf-code <ugur.firat.developer@gmail.com>
2022-05-28 14:16:33 +0300

    optimizing navigator

--- 00-mealstogo/App.js
+++ 00-mealstogo/App.js
@@ -1,54 +1,21 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import { Text } from 'react-native'
-// Screens
-import { RestaurantScreen as Restaurants } from './src/features/restaurants/screens/restaurants.screen.jsx'
-// Theme
+import React from 'react'
 import { ThemeProvider } from 'styled-components/native'
-import { theme } from './src/infrastructure/theme/index.js'
-// Fonts
+
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald'
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
-//
-import { SafeArea } from './src/components/utility/safe-area.component.jsx'
-// Providers
-import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context.jsx'
-import { LocationContextProvider } from './src/services/location/location.context.jsx'
-//
-import { Ionicons } from '@expo/vector-icons'
-//
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-const Tab = createBottomTabNavigator()
 
-const Settings = () => (
-    <SafeArea>
-        <Text>Settings</Text>
-    </SafeArea>
-)
-const Map = () => (
-    <SafeArea>
-        <Text>Map</Text>
-    </SafeArea>
-)
+import { theme } from './src/infrastructure/theme'
+import { Navigation } from './src/infrastructure/navigation'
 
-const TAB_ICON = {
-    Restaurants: 'md-restaurant',
-    Map: 'md-map',
-    Settings: 'md-settings',
-}
-
-// const tabBarIcon = ({ size, color }) => <Ionicons name={iconName} size={size} color={color} />
-const createScreenOptions = ({ route }) => {
-    const iconName = TAB_ICON[route.name]
-    return {
-        tabBarIcon: ({ size, color }) => <Ionicons name={iconName} size={size} color={color} />,
-    }
-}
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context'
+import { LocationContextProvider } from './src/services/location/location.context'
 
 export default function App() {
     const [oswaldLoaded] = useOswald({
         Oswald_400Regular,
     })
+
     const [latoLoaded] = useLato({
         Lato_400Regular,
     })
@@ -62,16 +29,7 @@ export default function App() {
             <ThemeProvider theme={theme}>
                 <LocationContextProvider>
                     <RestaurantsContextProvider>
-                        <NavigationContainer>
-                            <Tab.Navigator
-                                screenOptions={createScreenOptions}
-                                tabBarOptions={{ activeTintColor: 'tomato', inactiveTintColor: 'gray' }}
-                            >
-                                <Tab.Screen name='Restaurants' component={Restaurants} />
-                                <Tab.Screen name='Map' component={Map} />
-                                <Tab.Screen name='Settings' component={Settings} />
-                            </Tab.Navigator>
-                        </NavigationContainer>
+                        <Navigation />
                     </RestaurantsContextProvider>
                 </LocationContextProvider>
             </ThemeProvider>
