55ffb8e3bde075f11ecec1a97324acd2ca4376bb
uf-code <ugur.firat.developer@gmail.com>
2022-05-29 15:37:03 +0300

    making compact card clickable and adding navigation

--- 00-mealstogo/src/features/map/screens/map.screen.jsx
+++ 00-mealstogo/src/features/map/screens/map.screen.jsx
@@ -13,7 +13,7 @@ const Map = styled(MapView)`
     width: 100%;
 `
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
     const { location } = useContext(LocationContext)
     const { restaurants = [] } = useContext(RestaurantsContext)
 
@@ -51,7 +51,11 @@ export const MapScreen = () => {
                                 longitude: restaurant.geometry.location.lng,
                             }}
                         >
-                            <MapView.Callout>
+                            <MapView.Callout
+                                onPress={() => {
+                                    navigation.navigate('RestaurantDetail', { restaurant: restaurant })
+                                }}
+                            >
                                 <MapCallout restaurant={restaurant} />
                             </MapView.Callout>
                         </MapView.Marker>
