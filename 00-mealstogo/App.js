68f44a50339efcf415437512cf5c6241b4e432b2
uf-code <ugur.firat.developer@gmail.com>
2022-05-30 21:01:44 +0300

    fkin firebase gives me some real headache

--- 00-mealstogo/App.js
+++ 00-mealstogo/App.js
@@ -1,8 +1,6 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components/native'
-
-import * as firebase from 'firebase'
 
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald'
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
@@ -13,19 +11,6 @@ import { Navigation } from './src/infrastructure/navigation'
 import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context.jsx'
 import { LocationContextProvider } from './src/services/location/location.context.jsx'
 import { FavouritesContextProvider } from './src/services/favourites/favourites.context.jsx'
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: 'AIzaSyCcby9SHsm-bjwMupvbdfq8s1WVdesYumk',
-    authDomain: 'mealstogo-612bb.firebaseapp.com',
-    projectId: 'mealstogo-612bb',
-    storageBucket: 'mealstogo-612bb.appspot.com',
-    messagingSenderId: '126895014542',
-    appId: '1:126895014542:web:887bb48662130161f4ffdf',
-}
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
 
 export default function App() {
     const [oswaldLoaded] = useOswald({
