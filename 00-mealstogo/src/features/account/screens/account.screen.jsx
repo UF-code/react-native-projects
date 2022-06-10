8733132d8b97eaca85f32f21a47dbfd79a7b5f3c
uf-code <ugur.firat.developer@gmail.com>
2022-06-10 19:04:10 +0300

    implementing background styles into account screen

---
+++ 00-mealstogo/src/features/account/screens/account.screen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { AccountBackground, AccountCover } from '../components/account.styles.js'
+
+export const AccountScreen = () => {
+    return (
+        <AccountBackground>
+            <AccountCover />
+        </AccountBackground>
+    )
+}
--- 00-mealstogo/src/infrastructure/navigation/account.navigator.js
+++ 00-mealstogo/src/infrastructure/navigation/account.navigator.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+
 import { createStackNavigator } from '@react-navigation/stack'
+
+import { AccountScreen } from '../../features/account/screens/account.screen.jsx'
+import { LoginScreen } from '../../features/account/screens/login.screen.jsx'
+import { RegisterScreen } from '../../features/account/screens/register.screen.jsx'
 
 const Stack = createStackNavigator()
 
 export const AccountNavigator = () => (
-    <Stack.Navigator headerMode='none'>
-        <Stack.Screen
-            name='Main'
-            component={() => (
-                <View>
-                    <Text> Account Screen </Text>
-                </View>
-            )}
-        />
-        <Stack.Screen
-            name='Login'
-            component={() => (
-                <View>
-                    <Text> Login Screen </Text>
-                </View>
-            )}
-        />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Main' component={AccountScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
     </Stack.Navigator>
 )
