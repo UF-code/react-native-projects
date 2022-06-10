9b5b88182b50be218ed2b07ee236b0ce4d029d4f
uf-code <ugur.firat.developer@gmail.com>
2022-06-11 00:32:15 +0300

    styling the settings screen

--- 00-mealstogo/src/infrastructure/navigation/app.navigator.js
+++ 00-mealstogo/src/infrastructure/navigation/app.navigator.js
@@ -1,15 +1,13 @@
-import React, { useContext } from 'react'
+import React from 'react'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons'
-import { Text, Button } from 'react-native'
-
-import { SafeArea } from '../../components/utility/safe-area.component'
 
 import { RestaurantsNavigator } from './restaurants.navigator'
 import { MapScreen } from '../../features/map/screens/map.screen.jsx'
+import { SettingsScreen } from '../../features/settings/screens/settings.screen.jsx'
 
-import { AuthenticationContext } from '../../services/authentication/authentication.context'
+import { SettingsNavigator } from './settings.navigator.js'
 
 import { FavouritesContextProvider } from '../../services/favourites/favourites.context'
 import { LocationContextProvider } from '../../services/location/location.context'
@@ -21,16 +19,6 @@ const TAB_ICON = {
     Restaurants: 'md-restaurant',
     Map: 'md-map',
     Settings: 'md-settings',
-}
-
-const Settings = () => {
-    const { onLogout } = useContext(AuthenticationContext)
-    return (
-        <SafeArea>
-            <Text>Settings</Text>
-            <Button title='Logout' onPress={() => onLogout()} />
-        </SafeArea>
-    )
 }
 
 const createScreenOptions = ({ route }) => {
@@ -54,7 +42,7 @@ export const AppNavigator = () => (
                 >
                     <Tab.Screen name='Restaurants' component={RestaurantsNavigator} />
                     <Tab.Screen name='Map' component={MapScreen} />
-                    <Tab.Screen name='Settings' component={Settings} />
+                    <Tab.Screen name='Settings' component={SettingsNavigator} />
                 </Tab.Navigator>
             </RestaurantsContextProvider>
         </LocationContextProvider>
