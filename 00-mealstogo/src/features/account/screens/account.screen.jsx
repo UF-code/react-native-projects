3e835487adb9254024021c0162570deecd0df8a7
uf-code <ugur.firat.developer@gmail.com>
2022-06-10 19:28:34 +0300

    implementing buttons with navigation

--- 00-mealstogo/src/features/account/screens/account.screen.jsx
+++ 00-mealstogo/src/features/account/screens/account.screen.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
-import { AccountBackground, AccountCover } from '../components/account.styles.js'
+import { Button } from 'react-native-paper'
+import { Spacer } from '../../../components/spacer/spacer.component.jsx'
 
-export const AccountScreen = () => {
+import {
+    AccountBackground,
+    AccountContainer,
+    AccountCover,
+    AuthButton,
+} from '../components/account.styles.js'
+
+export const AccountScreen = ({ navigation }) => {
     return (
         <AccountBackground>
             <AccountCover />
+            <AccountContainer>
+                <Spacer />
+
+                <AuthButton
+                    icon='lock-open-outline'
+                    mode='contained'
+                    onPress={() => navigation.navigate('Login')}
+                >
+                    Login
+                </AuthButton>
+
+                <Spacer size='large' />
+
+                <AuthButton
+                    icon='lock-open-outline'
+                    mode='contained'
+                    onPress={() => navigation.navigate('Register')}
+                >
+                    Register
+                </AuthButton>
+
+                <Spacer />
+            </AccountContainer>
         </AccountBackground>
     )
 }
